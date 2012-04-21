55ac563d23e3edda22dabc5e34e0ca36f4c8370e
Michael Alexander <beefsack@gmail.com>
2012-04-21 11:55:25 +0800

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DesignPig::Application.config.secret_token = 'aa1b96963411c5ce7dcb51f0288b2cf9f1f4fab017b1857997165ae3c41f183d441c6186c88961d93a4fbe46a2679cc4cdd70ee0421b931c86d2bac743311a85'
