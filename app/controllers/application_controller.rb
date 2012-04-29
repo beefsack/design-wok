250a4ada15c2ec299ae261304fd8c6facc20ea40
Michael Alexander <beefsack@gmail.com>
2012-04-30 00:17:54 +0800

    Generated rdoc.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  # == Description
+  #
+  # Renders the errors in a nice format as an API.
+  #
+  # == Example output
+  #
+  #  errors:
+  #    messages:
+  #      name:
+  #        - 'must be present'
+  #    full_messages:
+  #      - 'Name must be present'
   def render_model_errors model
     @errors = model.errors
     render :controller => 'application', :action => 'errors',
